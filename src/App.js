7ebe7a114aeea5ea6a2a6fd92b7c3eccc2aa43b0
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-05-23 17:19:47 +0200

    Added Home component.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// Components imports
+import { Home } from "./components/homepage/home";
 import { Login } from "./components/login/login";
 import { SignUp } from "./components/sign-up/sign-up";
 
@@ -7,12 +9,14 @@ import {
   Route,
 } from "react-router-dom"
 
+
 function App() {
   return (
   <Router>
      <Routes>
        <Route path='/login' element={<Login/>}/>
        <Route path='/signup' element={<SignUp/>}/>
+       <Route path='/' element={<Home/>}/>
      </Routes>
   </Router>
   )
---
+++ src/components/homepage/home.js
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return (
+        <>
+        <h1> Hi, welcome to this project!</h1>
+        <ul> Current routes enabled:</ul>
+            <li> Signup == /signup </li>
+            <li> Login == /login </li>
+        </>
+    )
+}
