7c71417c657a8d92b6e7923927cda46638f2377b
alessandrodetommasi <4lessandrodetommasi@gmail.com>
2022-06-06 16:00:15 +0200

    merge Auth

--- src/components/landing-page/landingPage.js
+++ src/components/landing-page/landingPage.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Navbar } from "./navbar-landing-page/Navbar-landing";
 
 export const LandingPage = () => {
 
     return (
         <>
-        <Navbar/>
+        <h1> Hi, welcome to this project!</h1>
         <p>Current routes enabled:</p>
         <ul> 
             <li> LandingPage (This one) == / .</li>
