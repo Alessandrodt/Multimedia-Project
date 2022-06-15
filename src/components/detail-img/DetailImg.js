4c6e0b584392b7ee451f63c854babc3de282a5d7
alessandrodetommasi <4lessandrodetommasi@gmail.com>
2022-06-15 12:13:56 +0200

    marge main

--- src/components/detail-img/DetailImg.js
+++ src/components/detail-img/DetailImg.js
@@ -5,36 +5,36 @@ export const DetailImg = () => {
 
 
     return (
-        <main>
-                <div className="inner-box">
-                    <div className="img-box">
-                    </div>
-                    <div className="info-external-box">
-                        <div className="tag-box">
-                            <div className="text-box">
-                             <div className="title-box">
-                                    #Tag
-                                 </div>
-                             </div>   
-                         
-                        </div>
-                        <div className="description-box">
-                            <div className="text-box">
+        <>
+            <div className="inner-box">
+                <div className="img-box">
+                </div>
+                <div className="info-external-box">
+                    <div className="tag-box">
+                        <div className="text-box">
                             <div className="title-box">
-                                    Description
+                                #Tag
                                 </div>
-                             </div>
-                        </div>
+                            </div>  
+                    </div>
+                    <div className="description-box">
+                        <div className="text-box">
+                        <div className="title-box">
+                                Description
+                            </div>
+                            </div>
                     </div>
                 </div>
-                <div className="external-button-box">
-                            <div className="button-box-sx">
-                                <button className="button-action">Modify</button>
-                            </div>  
-                            <div className="button-box">
-                                <button className="button-action">Delete</button>
-                            </div>
+            </div>
+            <div className="external-button-box">
+                        <div className="button-box-sx">
+                            <button className="button-action">Modify</button>
+                        </div>  
+                        <div className="button-box">
+                            <button className="button-action">Delete</button>
                         </div>
-        </main>
+                    </div>
+        </>
+
     )
 }
