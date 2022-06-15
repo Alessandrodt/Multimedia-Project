65598e78ee12987a2e67e5a021506b924a8b74f8
alessandrodetommasi <4lessandrodetommasi@gmail.com>
2022-06-15 12:24:17 +0200

    merge fixed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,12 @@
 import logo from "../../images/picsmi.png";
 import { InputWithButton } from "../search/Srcbar";
-// import avatar from "../../images/avatar.svg";
+import avatar from "../../images/avatar.svg";
 import group from "../../images/group.svg";
 import { Link } from "react-router-dom";
 
+
+
 export const Navbar = () => {
-const user = JSON.parse(sessionStorage.getItem('user'));
  
     return (
         <>
@@ -17,14 +18,11 @@ const user = JSON.parse(sessionStorage.getItem('user'));
                     <div className="srcnav">
                         <InputWithButton className="bar"/>
                     </div>
-                    <h4>Hello {user.first_name} {user.last_name}!</h4>
                     <div className="sign">
-                        <Link to={`/users/${user.id}`}>
-                            <button>
-                                <img className="icong" src={`http://smear-backend.test//images/avatars/${user.avatar.name}`} title="Profilo" alt="company logo" />
-                            </button>
-                        </Link>
-                        <button>
+                        <button className="button-action">
+                        <img className="icong" src={avatar} title="Profilo" alt="company logo" />
+                        </button>
+                        <button className="button-action">
                         <img className="icong" src={group} title="Groups" alt="company logo" />
                         </button>
                     </div>
