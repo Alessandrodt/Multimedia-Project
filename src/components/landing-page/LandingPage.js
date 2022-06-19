e5dc1a32479c91d2317e7a8deaedbadf6ad9455b
Alessandro Catucci <alessandrocatucci@AirdiAlessandro.home>
2022-06-19 12:31:49 +0200

    add link logo to homepage

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -4,6 +4,7 @@ import { useModals } from "@mantine/modals";
 import { Login } from "../../components/login/Login";
 //import landingPageServices from "../../services/landingPageServices";
 
+import { Link } from "react-router-dom";
 
 import { SignUp } from "../../components/sign-up/Signup";
 
@@ -33,7 +34,9 @@ export const LandingPage = () => {
             <nav>
                 <div className="wrapper-nav">
                     <div className="logo">
-                        <img src={logo} title="logo smi" alt="company logo" />
+                        <Link to="/">
+                            <img src={logo} title="logo smi" alt="company logo" />
+                        </Link>
                     </div>
                     <div className="signUp">
                         <span onClick={openContentModal} className="avatar">
