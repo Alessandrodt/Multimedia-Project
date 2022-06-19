9960055c01803ba77d7f041c392dd4f1496877e5
alessandrodetommasi <4lessandrodetommasi@gmail.com>
2022-06-19 20:50:33 +0200

    fixed

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -5,8 +5,8 @@ import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
-import i18next from 'i18next'
-import cookies from 'js-cookie'
+import i18next from "i18next";
+import {cookies} from 'js-cookie';
 import { useEffect } from "react";
 
 i18n
