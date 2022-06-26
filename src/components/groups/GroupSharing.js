58955437ebe14c6aabe6fa213a938fe13f979405
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-06-26 19:39:06 +0200

    removeFolderToGroup (Gives 401 too, WIP)

--- src/components/groups/GroupSharing.js
+++ src/components/groups/GroupSharing.js
@@ -7,6 +7,7 @@ import { Button } from "@mantine/core";
 
 export const GroupSharing = () => {
     const [folders, setFolders] = useState([]);
+    const [sharedFolders, setSharedFolders] = useState([]);
     const { userId, groupId } = useParams();
     
     useEffect(() => {
@@ -15,17 +16,44 @@ export const GroupSharing = () => {
         });
     }, [userId])
 
-    const addFolderToGroup = (folderId) => {
-        folderSharingServices.addFolderToGroup(groupId, folderId)
+    const addFolderToGroup = (f) => {
+        folderSharingServices.addFolderToGroup(groupId, f.id)
         .then(res => {
-            console.log(res.data);
+            console.log(`${f.name} is now being shared with this group`);
+            setSharedFolders(sharedFolders.concat(res.data));
+        })
+        .catch(err => {
+            if (err.response.status = 401) {
+                console.log('User is unauthorized')
+            } else if (err.response.status === 403) {
+                console.log('This action is forbidden')
+            } else {
+                console.log('Not found')
+            }
         });
-    }
+    };
+
+    const removeFolderFromGroup = (f) => {
+        folderSharingServices.removeFolderFromGroup(groupId, f.id)
+        .then(res => {
+            console.log(`${f.name} will not be shared with this group anymore`);
+            setSharedFolders(res.data);
+        })
+        .catch(err => {
+            if (err.response.status = 401) {
+                console.log('User is unauthorized')
+            } else if (err.response.status === 403) {
+                console.log('This action is forbidden')
+            } else {
+                console.log('Not found')
+            }
+        });
+    };
 
     return (
         <>
         <h1> GROUP IMAGE SHARING </h1>
-            <h2> You should be able to only see the folders YOU are sharing to others in this section </h2>
+            <h3> This needs to go in a modal </h3>
                 <ul>
                     {folders.map(folder => {
                         return (
@@ -33,11 +61,16 @@ export const GroupSharing = () => {
                                 <br />
                                 <p> {folder.name} ( FId = {folder.id} ) </p>
                                 <br />
-                                <Button compact onClick={() => addFolderToGroup(folder.id)}> Add to Group </Button> <Button compact> Remove from Group </Button>
+                                <Button compact onClick={() => addFolderToGroup(folder)}> Add to Group </Button> 
+                                <Button compact onClick={() => removeFolderFromGroup(folder)}> Remove from Group </Button>
                                 <br />
                             </li>
                         );
                     })}
+                </ul>
+            <h3> This is the section where you should see if a folder is added or not but we don't have the facilities for this </h3>
+                <ul>
+                    {}
                 </ul>
         </>
     )
