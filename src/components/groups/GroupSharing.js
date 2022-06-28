a1715a985d297c364a7db22fdf8e5b13a8a0e3b0
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-06-28 18:15:26 +0200

    Moved modal to GroupSharing, it works now.

--- src/components/groups/GroupSharing.js
+++ src/components/groups/GroupSharing.js
@@ -1,20 +1,94 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import { AddFolderModal } from "./AddFolderModal";
+import { useModals } from "@mantine/modals";
+import { Button } from "@mantine/core";
+
+import foldersServices from "../../services/foldersServices";
+import folderSharingServices from "../../services/folderSharingServices";
+
 
 export const GroupSharing = () => {
     const [sharedFolders, setSharedFolders] = useState([]);
-    
+    const [folders, setFolders] = useState([]);
+    const { groupId, userId } = useParams();
+    const modal = useModals();
+
+    useEffect(() => {
+        foldersServices.getFolder(userId).then((response) => {
+          setFolders(response.data);
+        });
+    }, [userId]);
+
+    const addFolderToGroup = (f) => {
+        folderSharingServices.addFolderToGroup(groupId, f.id)
+        .then(res => {
+            console.log(res)
+            console.log(`${f.name} is now being shared with this group`);
+            setSharedFolders(sharedFolders.concat(res.data));
+            modal.closeModal();
+        })
+        .catch(err => {
+            if (err.response.status === 401) {
+                console.log('User is unauthorized')
+            } else if (err.response.status === 403) {
+                console.log('This action is forbidden')
+            } else {
+                console.log('Not found')
+            }
+        });
+    };
+    console.log(sharedFolders);
+
+    const removeFolderFromGroup = (f) => {
+        folderSharingServices.removeFolderFromGroup(groupId, f.folder.id)
+        .then(res => {
+            console.log(res)
+            console.log(`${f.folder.name} will not be shared with this group anymore`);
+            setSharedFolders(sharedFolders.filter((f) => f.id !== f.folder.id));
+        })
+        .catch(err => {
+            if (err.response.status === 401) {
+                console.log('User is unauthorized')
+            } else if (err.response.status === 403) {
+                console.log('This action is forbidden')
+            } else {
+                console.log('Not found')
+            }
+        });
+    };
+
+    const openContentAddModal = () => {
+        modal.openModal({
+          title: "Choose your folder's name:",
+          children:  
+            <ul>
+                {folders.map(folder => {
+                    return (
+                        <li key={folder.id}> 
+                            <br />
+                            <p> {folder.name} ( FId = {folder.id} ) </p>
+                            <br />
+                            <Button compact onClick={() => addFolderToGroup(folder)}> Add to Group </Button> 
+                            <br />
+                        </li>
+                    );
+                })}
+            </ul>,
+        });
+      };
+
     return (
         <>
         <h1> GROUP IMAGE SHARING </h1>
-            <AddFolderModal sharedFolders={sharedFolders} setSharedFolders={setSharedFolders} />
+        <Button onClick={() => {openContentAddModal()}}> Choose Folders </Button>
             <h3> This is the section where you should see if a folder is added or not but we don't have the facilities for this </h3>
             <ul> 
                 {sharedFolders?.map(sharedFolder => {
                     return (
-                        <li key={sharedFolder.id} >
-                            {sharedFolder.name}
+                        <li key={sharedFolder.folder.id} >
+                            {sharedFolder.folder.name}
+                            <Button compact onClick={() => removeFolderFromGroup(sharedFolder)}> Remove from Group </Button>
                         </li>
                     )
                 })}
