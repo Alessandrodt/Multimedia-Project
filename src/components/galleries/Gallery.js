37d39991abdf79ae254362b1e6c89415ff914767
Arianna Poverini <arianna.poverini@gmail.com>
2022-07-06 01:22:35 +0200

    fix tags breaking when no images found

--- src/components/galleries/Gallery.js
+++ src/components/galleries/Gallery.js
@@ -94,7 +94,9 @@ export function Gallery({ folderId, userId, searchParams, newUploadImages }) {
           <Masonry columns={[1, 2, 3, 4]} spacing={2} loading="lazy">
             {galleryImages.map((e) => (
               <div className="box-gallery-home" key={e.id}>
-                <span className="title-tags">{e.tags.join(", ")}</span>
+                <span className="title-tags">
+                  {e.tags ? e.tags.join(", ") : ""}
+                </span>
                 <Card img={e.urls} idImage={e.id} key={e.id}></Card>
               </div>
             ))}
