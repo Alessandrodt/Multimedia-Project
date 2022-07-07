84b6e264afe4dee62995959b3722783e8ba83085
Arianna Poverini <arianna.poverini@gmail.com>
2022-07-07 10:00:59 +0200

    join on gallery tags

--- src/components/galleries/Gallery.js
+++ src/components/galleries/Gallery.js
@@ -94,7 +94,9 @@ export function Gallery({ folderId, userId, searchParams, newUploadImages }) {
           <Masonry columns={[1, 2, 3, 4]} spacing={2} loading="lazy">
             {galleryImages.map((e) => (
               <div className="box-gallery-home" key={e.id}>
-                <span className="title-tags">{e.tags ? e.tags : ""}</span>
+                <span className="title-tags">
+                  {e.tags ? e.tags.join(", ") : ""}
+                </span>
                 <Card img={e.urls} idImage={e.id} key={e.id}></Card>
               </div>
             ))}
@@ -104,5 +106,3 @@ export function Gallery({ folderId, userId, searchParams, newUploadImages }) {
     </div>
   );
 }
-
-// {e.tags ? e.tags.join(", ") : ""}
