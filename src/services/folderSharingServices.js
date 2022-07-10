8bfb95aab541f5aef077a89f6c8320b65f89f4b9
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-07-10 21:10:24 +0200

    Added comments to services.

--- src/services/folderSharingServices.js
+++ src/services/folderSharingServices.js
@@ -1,14 +1,32 @@
 import axios from './axios';
 
 const addFolderToGroup = (groupId, folderID) => {
+ /**
+ * This method is used to link a Folder instance to a Group one, allowing it to be shared.
+ * @param {string} groupId - The group id, retrieved through the useParams hook.
+ * @param {string} folderId - The folder id, retrieved through the useParams hook.
+ * @returns A folder instance.
+ */
     return axios.post(`https://smi-laravel.fly.dev/api/v1/groups/${groupId}/folders/${folderID}`)
 }
 
 const removeFolderFromGroup = (groupId, folderID) => {
+ /**
+ * This method is used to sever the link a Folder instance to a Group one, allowing it to be shared.
+ * @param {string} groupId - The group id, retrieved through the useParams hook.
+ * @param {string} folderId - The folder id, retrieved through the useParams hook.
+ * @returns The response is not used in the function that calls this method.
+ */
     return axios.delete(`https://smi-laravel.fly.dev/api/v1/groups/${groupId}/folders/${folderID}`)
 }
 
 const getSharedFolders = (userId, groupId) => {
+ /**
+ * This method is used to get the folders that are linked in a given Group instance.
+ * @param {string} groupId - The group id, retrieved through the useParams hook.
+ * @param {string} folderId - The folder id, retrieved through the useParams hook.
+ * @returns An array of folder instances.
+ */
     return axios.get(`https://smi-laravel.fly.dev/api/v1/users/${userId}/folders?filter[groups]=${groupId}&include=folders,groups,groups.users`);
 }
 
--- src/services/groupsServices.js
+++ src/services/groupsServices.js
@@ -1,22 +1,41 @@
 import axios from "../services/axios";
 
-// Method to retrieve the groups created by the user from the backend.
+
 const getUserGroups = (id) => {
+ /**
+ *  This method is used to retrieve groups where an user is present.
+ * @param {string} id - User id, retrieved from the user object in the sessionStorage.
+ * @returns An array of Group object instances.
+ */
     return axios.get(`https://smi-laravel.fly.dev/api/v1/users/${id}/groups`);
 };
 
-// Method to create an empty group on the backend.
 const createGroup = (id, newGroup) => {
+ /**
+ *  This method is used to create a Group instance on the backend.
+ * @param {string} id - User id, retrieved from the user object in the sessionStorage.
+ * @param {object} newGroup - A group instance, with a single key 'name'.
+ * @returns A single new Group instance.
+ */
     return axios.post(`https://smi-laravel.fly.dev/api/v1/users/${id}/groups`, newGroup);
 };
 
-// Method to search a user via their email address.
 const searchUser = (email) => {
+ /**
+ * Method to search a user via their email address. The email param is filtered in the backend.
+ * @param {string} email - User email, retrieved from e.currentTarget.value in the Searchbar text input.
+ * @returns An array of User objects.
+ */
     return axios.get(`https://smi-laravel.fly.dev/api/v1/users`, email);
 };
 
-// Method to add user to the group. 
 const addUser = (group, user) => {
+ /**
+ * Method to add an user to the group. 
+ * @param {string} group - The group id, retrieved through the useParams hook.
+ * @param {string} user - User id, retrieved from the user object in the sessionStorage. 
+ * @returns A user istance.
+ */
     return axios.post(`https://smi-laravel.fly.dev/api/v1/groups/${group}/users/${user}`);
 };
 
