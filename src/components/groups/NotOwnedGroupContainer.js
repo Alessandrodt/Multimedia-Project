6703cfce47eefcd3aaa5f2493e1bbddad01064ae
Arianna Poverini <arianna.poverini@gmail.com>
2022-07-11 15:10:15 +0200

    clickable group elements

--- src/components/groups/NotOwnedGroupContainer.js
+++ src/components/groups/NotOwnedGroupContainer.js
@@ -10,27 +10,26 @@ import groupeople from "../../images/groupeople.svg";
 // GroupContainer is a simple component used to store the non owned group infos displayed in the Groups page.
 export const NotOwnedGroupContainer = ({ groupName, sharedGroup }) => {
   return (
-    <Paper p="md" className="paper-groups" radius="md" shadow="xs" withBorder>
-      <div className="group-name-svg">
-        <img src={groupeople} alt=""></img>
-        <Text
-          className="txt-group"
-          align="center"
-          size="lg"
-          weight={500}
-          mt="md"
-        >
-          {groupName}
-        </Text>
-      </div>
-      <div className="button-groups-action">
-      <Link className="link-groups" to={sharedGroup}>
+    <Link className="link-groups" to={sharedGroup}>
+      <Paper p="md" className="paper-groups" radius="md" shadow="xs" withBorder>
+        <div className="group-name-svg">
+          <img src={groupeople} alt=""></img>
+          <Text
+            className="txt-group"
+            align="center"
+            size="lg"
+            weight={500}
+            mt="md"
+          >
+            {groupName}
+          </Text>
+        </div>
+        <div className="button-groups-action">
           <Button name="group-button" fullWidth mt="md">
             <img src={folderWithElement} alt=""></img>
           </Button>
-      </Link>
-      </div>
-     
-    </Paper>
+        </div>
+      </Paper>
+    </Link>
   );
 };
